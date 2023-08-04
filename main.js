753b0fb0f8f31c92b8ae59e19f24d17bac18e9cf
AyushGit1253 <ayush1253khadatkar@gmail.com>
2023-08-04 23:14:38 +0530

    axios

--- main.js
+++ main.js
@@ -6,16 +6,23 @@ var filter=document.getElementById('filter')
 var userItem=document.getElementById('items')
 
 form.addEventListener('submit',onSubmit)
-deleteItem.addEventListener('click',onDelete)
-editItem.addEventListener('click',onEdit)
+
 filter.addEventListener('keyup',search)
 
 function onSubmit(e){
     e.preventDefault()
 
+    deleteItem.addEventListener('click',onDelete)
+    editItem.addEventListener('click',onEdit)
+    
     var expense=document.getElementById('expense').value
     var desc=document.getElementById('desc').value
     var category=document.getElementById('category').value
+    var obj={
+        exp:expense,
+        des:desc,
+        cat:category
+    }
 
     if(expense===''||desc===''||category===''){
         msg.innerHTML='Please enter all fields'
@@ -23,11 +30,7 @@ function onSubmit(e){
         setTimeout(()=>msg.remove(),3000)
     }
 
-    var obj={
-        exp:expense,
-        des:desc,
-        cat:category
-    }
+   
 
     var li=document.createElement('li')
     li.className='list-group-item'
@@ -43,13 +46,19 @@ function onSubmit(e){
     btnE.innerHTML='EditExpense'
     li.appendChild(btnE) 
 
-
     userItem.appendChild(li)
 
-    localStorage.setItem(obj.des,JSON.stringify(obj))
+    //localStorage.setItem(obj.des,JSON.stringify(obj))
+    //using axios
+    axios.post("https://crudcrud.com/api/3c978aa96d0a40c48e0e38924a0ff967/appointment",obj)
+    .then((response)=>{
+        console.log(response.data)
+    })
+    .catch((err)=> console.log(err))
     document.getElementById('expense').value=''
     document.getElementById('desc').value=''
     document.getElementById('category').value=''
+    
 
 
 }
@@ -67,39 +76,12 @@ function onDelete(e){
     }
 }
 
-function onDelete1(e){
-    e.preventDefault()
 
-    if(e.target.classList.contains('delete1')){
-        if(confirm('Are you sure')){
-            var li=e.target.parentElement
-            var desc=li.textContent.split('-')[1]
-            userItem.removeChild(li)
-            localStorage.removeItem(desc)
-        }
-    }
-}
 
 function onEdit(e){
     e.preventDefault()
 
     if(e.target.classList.contains('edit')){
-        
-            var li=e.target.parentElement
-            var expense=li.textContent.split('-')[0]
-            document.getElementById('expense').value=expense
-            var desc=li.textContent.split('-')[1]
-            document.getElementById('desc').value=desc
-            userItem.removeChild(li)
-            localStorage.removeItem(desc)
-        
-    }
-}
-
-function onEdit1(e){
-    e.preventDefault()
-
-    if(e.target.classList.contains('edit1')){
         
             var li=e.target.parentElement
             var expense=li.textContent.split('-')[0]
@@ -124,20 +106,20 @@ function onEdit1(e){
     li.appendChild(document.createTextNode(exp + '-' + des + '-' + cat)) 
 
     var btn=document.createElement('button') 
-    btn.className='btn btn-danger btn-sm float-right delete1'
+    btn.className='btn btn-danger btn-sm float-right delete'
     btn.innerHTML='DeleteExpense'
     li.appendChild(btn)
 
     var btnE=document.createElement('button')
-    btnE.className='btn btn-primary btn-sm float-right edit1'
+    btnE.className='btn btn-primary btn-sm float-right edit'
     btnE.innerHTML='EditExpense'
     li.appendChild(btnE) 
 
 
     userItem.appendChild(li)
 
-    btn.addEventListener('click',onDelete1)
-    btnE.addEventListener('click',onEdit1)
+    btn.addEventListener('click',onDelete)
+    btnE.addEventListener('click',onEdit)
 
     } 
 
